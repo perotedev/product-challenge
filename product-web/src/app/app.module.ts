59a1585eff6fc82c277b16d614190fae30dc1fd2
rodrigoperote <rodripema@hotmail.com>
2021-12-01 18:42:58 -0400

    [FRONTEND]feat: #1 Criada a estrutura básica dos componentes

--- product-web/src/app/app.module.ts
+++ product-web/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ProductsComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
